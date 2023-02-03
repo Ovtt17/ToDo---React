7002b880534d937efc846f2ff519c53ec2fd7bed
Ovett <ovettmora17@gmail.com>
2023-02-03 16:40:24 -0600

    tiempo de carga y error en caso de que suceda

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,65 @@
-import React from "react";
-import {TodoCounter} from './TodoCounter';
-import {TodoSearch} from './TodoSearch';
-import {TodoList} from './TodoList';
-import {TodoItem} from './TodoItem';
-import {CreateTodoButton} from './CreateTodoButton'
-import '../styles/App.css';
+import React from 'react';
+import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  {text: 'Study JavaScript', completed: true },
-  {text: 'Learn React', completed: false },
-  {text: 'Wash the dishes', completed: false },
-  {text: 'Learn English', completed: false },
-]
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: true },
+//   { text: 'LALALALAA', completed: false },
+// ];
+
+function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
+  
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      } catch(error) {
+        setError(error);
+      }
+    }, 1000);
+  });
+  
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error) {
+      setError(error);
+    }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue, setSearchValue]= React.useState('');
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -24,51 +68,40 @@ function App() {
 
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
-  }else {
+  } else {
     searchedTodos = todos.filter(todo => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
     });
-    
   }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
-    newTodos[todoIndex].completed= true;
-    setTodos(newTodos);
-  }
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
-  }
+    saveTodos(newTodos);
+  };
+
   return (
-    <React.Fragment>
-      <TodoCounter 
-          total = {totalTodos}
-          completed = {completedTodos}
-        />
-
-        <TodoSearch
-          searchValue={searchValue}
-          setSearchValue = {setSearchValue}
-        />
-        <TodoList>
-          {searchedTodos.map(todo => (
-            <TodoItem  
-            key= {todo.text} 
-            text= {todo.text}
-            completed= {todo.completed}
-            onComplete = {() => completeTodo(todo.text)}
-            onDelete = {() => deleteTodo(todo.text)}
-            />
-          ))}
-        </TodoList>
-
-        <CreateTodoButton />
-    </React.Fragment>
+    <AppUI
+      loading={loading}
+      error={error}
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
